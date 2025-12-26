2a14a2d1cde28777679547abc888f795a404f3d0
ArthurOttevaere <arthur.ottevaere@student.uclouvain.be>
2025-12-27 00:15:02 +0100

    Create main.py
    To be able to call all the functions in order to execute the project.

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Launching script for the project of Web Mining
+This script alloww the launching of the diffrent steps of the project (scraping, text mining and link analysis).
+It works by calling all of the required functions.
+"""
+
+def main():
+    print("🚀 Processing the launching...")
+    
+    # Here : call to scraping, text mining and link analysis : 
+    # 1. Scraping
+    # 2. Text mining 
+    # 3. Link analysis
+    
+    print("✅ Finished.")
+
+if __name__ == "__main__":
+    main()
